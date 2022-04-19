3900c1affde1a9ea788f9c42c2b7b019b2d20105
Sandiprnm <sandipurnama1603@gmail.com>
2022-04-19 10:37:35 +0700

    tugas

---
+++ index.js
@@ -0,0 +1,19 @@
+//Persegi
+function persegi(sisi) {
+    console.log("Hasilnya " + sisi * sisi)
+}
+persegi(4)
+
+
+//jajar genjang
+function jajargenjang(alas, tinggi) {
+    console.log("Hasilnya " + alas * tinggi);
+}
+jajargenjang(6,9)
+
+
+//Persegi panjang
+function persegipanjang(panjang, lebar) {
+    console.log("Hasilnya " + panjang * lebar);
+}
+persegipanjang(9,2)
